b3da4c6d8802ddbacdd1fe118f4b2b98e75717ee
KazarinaT <tetianakazarina@gmail.com>
2021-06-19 11:10:22 +0300

    Class Work 3_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+
+
+function Form(props) {
+ 
+let  nameRef = React.createRef();
+
+ let  lastNameRef = React.createRef();
+
+
+  const handlerSubmit = (event) => {
+  event.preventDefault();
+ props.onclic(nameRef.current.value,lastNameRef.current.value)
+  
+  
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+  <form onSubmit={handlerSubmit}>
+  <div className="formElement"> 
+   <input  type="text" placeholder="Ваше имя" ref={nameRef} required />  
+  </div>
+
+  <div className="formElement">
+  <input type="text" placeholder="Ваша фамилия" ref={lastNameRef} required />
+  </div>
+
+  <div className="formElement">
+  <input type="submit" value="занести " />
+  </div>
+  </form>
+
+
+  </div>
+  );
+  }
+  function MyyBlock(props) {
+    return (
+      <div>
+       <div >
+        {props.Mydan}  
+      </div>
+
+    <div>
+    {props.MyLast}
+    </div>
+    </div>
+    );
+   }
+
+   export default function App() {
+      const [name,setname]=useState(" ");
+      const [lastname, setlastname]=useState(" ");
+
+      let Myfun=(newName,newlasnam)=>{setname(newName);setlastname(newlasnam)}
+      return (
+      <div >  
+         <Form onclic={Myfun} /> 
+         <MyyBlock Mydan={name} MyLast={lastname} ></MyyBlock> 
     </div>
   );
 }
 
-export default App;
+
